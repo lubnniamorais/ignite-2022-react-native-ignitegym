c548a3f1b37b5cac721da9d53a56b41918c355c8
lubnniamorais <lubnnia@gmail.com>
2025-03-17 22:11:16 -0300

    Add library biome

--- App.tsx
+++ App.tsx
@@ -1,27 +1,32 @@
 import { StatusBar } from 'react-native';
-import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
-import { Center, GluestackUIProvider,  Text } from '@gluestack-ui/themed';
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto';
+import { Center, GluestackUIProvider, Text } from '@gluestack-ui/themed';
 
 import { config } from './config/gluestack-ui.config';
 
 import { Loading } from '@components/Loading';
 
 export default function App() {
-  const [fontsLoaded] = useFonts({Roboto_400Regular, Roboto_700Bold});
-  
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
   return (
     <GluestackUIProvider config={config}>
-      
-        <StatusBar barStyle='light-content' backgroundColor="transparent" translucent />
-        {fontsLoaded ? (
-          <Center flex={1} bg="$gray700">
-            <Text>Home</Text>
-          </Center>
-          ) : ( 
-            <Loading />
-          )
-        }
-      
+      <StatusBar
+        barStyle='light-content'
+        backgroundColor='transparent'
+        translucent
+      />
+      {fontsLoaded ? (
+        <Center flex={1} bg='$gray700'>
+          <Text>Home</Text>
+        </Center>
+      ) : (
+        <Loading />
+      )}
     </GluestackUIProvider>
   );
 }
