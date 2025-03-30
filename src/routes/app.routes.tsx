f46b9e104d572c3d634c36fbd897c810f6145d4f
lubnniamorais <lubnnia@gmail.com>
2025-03-30 18:49:19 -0300

    feat: Create app.routes and add icon in bottom tab

---
+++ src/routes/app.routes.tsx
@@ -0,0 +1,71 @@
+import {
+  createBottomTabNavigator,
+  type BottomTabNavigationProp,
+} from '@react-navigation/bottom-tabs';
+
+import { gluestackUIConfig } from '../../config/gluestack-ui.config';
+
+import HomeSvg from '@assets/home.svg';
+import HistorySvg from '@assets/history.svg';
+import ProfileSvg from '@assets/profile.svg';
+
+import { Home } from '@screens/Home';
+import { Exercise } from '@screens/Exercise';
+import { Profile } from '@screens/Profile';
+import { History } from '@screens/History';
+
+type AppRoutes = {
+  home: undefined;
+  exercise: undefined;
+  profile: undefined;
+  history: undefined;
+};
+
+export type AppNavigatorRoutesProps = BottomTabNavigationProp<AppRoutes>;
+
+const { Navigator, Screen } = createBottomTabNavigator<AppRoutes>();
+
+export function AppRoutes() {
+  const { tokens } = gluestackUIConfig;
+
+  const iconSize = tokens.space[6];
+
+  return (
+    <Navigator screenOptions={{ headerShown: false, tabBarShowLabel: false }}>
+      {/* A propriedade headerShown: false remove o cabeçalho das telas */}
+      {/* A sequência disposta aqui irá influenciar em como o 
+      menu aparecerá na aplicação */}
+      <Screen
+        name='home'
+        component={Home}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <HomeSvg fill={color} width={iconSize} height={iconSize} />
+          ),
+        }}
+      />
+
+      <Screen
+        name='history'
+        component={History}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <HistorySvg fill={color} width={iconSize} height={iconSize} />
+          ),
+        }}
+      />
+
+      <Screen
+        name='profile'
+        component={Profile}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <ProfileSvg fill={color} width={iconSize} height={iconSize} />
+          ),
+        }}
+      />
+
+      <Screen name='exercise' component={Exercise} />
+    </Navigator>
+  );
+}
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,6 +2,7 @@ import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
 import { Box } from '@gluestack-ui/themed';
 
 import { AuthRoutes } from './auth.routes';
+import { AppRoutes } from './app.routes';
 
 import { gluestackUIConfig } from '../../config/gluestack-ui.config';
 
@@ -13,7 +14,7 @@ export function Routes() {
     // Essa box é para assegurar a cor de fundo da aplicação
     <Box flex={1} bg='$gray700'>
       <NavigationContainer theme={theme}>
-        <AuthRoutes />
+        <AppRoutes />
       </NavigationContainer>
     </Box>
   );
