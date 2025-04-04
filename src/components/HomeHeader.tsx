729115e51345c5673e021b8266e1d83bfe21ea04
lubnniamorais <lubnnia@gmail.com>
2025-04-04 10:21:16 -0300

    feat: Create UserPhoto component

--- src/components/HomeHeader.tsx
+++ src/components/HomeHeader.tsx
@@ -1,9 +1,19 @@
-import { Heading, HStack, Text, VStack } from '@gluestack-ui/themed';
+import { Heading, HStack, Text, VStack, Icon } from '@gluestack-ui/themed';
+import { LogOut } from 'lucide-react-native';
+
+import { UserPhoto } from './UserPhoto';
 
 export function HomeHeader() {
   return (
-    <HStack bg='$gray600' pt='$16' pb='$5' px='$8' alignItems='center'>
-      <VStack>
+    <HStack bg='$gray600' pt='$16' pb='$5' px='$8' alignItems='center' gap='$4'>
+      <UserPhoto
+        source={{ uri: 'https://github.com/lubnniamorais.png' }}
+        w='$16'
+        h='$16'
+        alt='Imagem do usuário'
+      />
+
+      <VStack flex={1}>
         <Text color='$gray100' fontSize='$sm'>
           Olá,
         </Text>
@@ -11,6 +21,8 @@ export function HomeHeader() {
           Lubnnia Morais
         </Heading>
       </VStack>
+
+      <Icon as={LogOut} color='$gray200' size='xl' />
     </HStack>
   );
 }
