52e5c0ce2db6b60230797067c5ff6ba3b6a3e9c5
lubnniamorais <lubnnia@gmail.com>
2025-04-06 16:13:28 -0300

    feat: Add FlatList to list groups

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,14 +1,40 @@
-import { VStack } from '@gluestack-ui/themed';
+import { useState } from 'react';
+import { FlatList } from 'react-native';
+import { VStack, HStack } from '@gluestack-ui/themed';
 
 import { HomeHeader } from '@components/HomeHeader';
 import { Group } from '@components/Group';
 
 export function Home() {
+  const [groups, setGroups] = useState([
+    'Costas',
+    'Bíceps',
+    'Tríceps',
+    'Ombro',
+  ]);
+  const [groupSelected, setGroupSelected] = useState('Costas');
+
   return (
     <VStack flex={1}>
       <HomeHeader />
 
-      <Group name='Costas' isActive={false} />
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => (
+          <Group
+            name={item}
+            isActive={groupSelected === item}
+            onPress={() => setGroupSelected(item)}
+          />
+        )}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={{
+          paddingHorizontal: 32,
+        }}
+        style={{ marginVertical: 40, maxHeight: 44, minHeight: 44 }}
+      />
     </VStack>
   );
 }
