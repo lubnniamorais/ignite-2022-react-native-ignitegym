feabaf401d3a5d6e476b973d4960a17205755ff4
lubnniamorais <lubnnia@gmail.com>
2025-04-11 09:23:35 -0300

    chore: Add UserPhoto componente in Profile screen

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,9 +1,35 @@
-import { Center, Text } from '@gluestack-ui/themed';
+import { ScrollView, TouchableOpacity } from 'react-native';
+import { Center, Text, VStack } from '@gluestack-ui/themed';
+
+import { ScreenHeader } from '@components/ScreenHeader';
+import { UserPhoto } from '@components/UserPhoto';
 
 export function Profile() {
   return (
-    <Center flex={1}>
-      <Text color='$white'>Profile</Text>
-    </Center>
+    <VStack flex={1}>
+      <ScreenHeader title='Perfil' />
+
+      <ScrollView contentContainerStyle={{ paddingBottom: 36 }}>
+        <Center mt='$6' px='$10'>
+          <UserPhoto
+            source={{ uri: 'https:github.com/lubnniamorais.png' }}
+            alt='Foto do usuário'
+            size='xl'
+          />
+
+          <TouchableOpacity>
+            <Text
+              color='$green500'
+              fontFamily='$heading'
+              fontSize='$md'
+              mt='$2'
+              mb='$8'
+            >
+              Alterar foto
+            </Text>
+          </TouchableOpacity>
+        </Center>
+      </ScrollView>
+    </VStack>
   );
 }
