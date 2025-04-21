a4d64d1f352863b9773d220bc0cd99cfb4ba59c6
lubnniamorais <lubnnia@gmail.com>
2025-04-21 17:18:50 -0300

    feat: Create ToastMessage component

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,31 +1,63 @@
 import { useState } from 'react';
 import { ScrollView, TouchableOpacity } from 'react-native';
-import { Center, Heading, Text, VStack } from '@gluestack-ui/themed';
+import { Center, Heading, Text, VStack, useToast } from '@gluestack-ui/themed';
 import * as ImagePicker from 'expo-image-picker';
+import * as FileSystem from 'expo-file-system';
 
 import { ScreenHeader } from '@components/ScreenHeader';
 import { UserPhoto } from '@components/UserPhoto';
 import { Input } from '@components/Input';
 import { Button } from '@components/Button';
+import { ToastMessage } from '@components/ToastMessage';
 
 export function Profile() {
   const [userPhoto, setUserPhoto] = useState(
     'https:github.com/lubnniamorais.png'
   );
 
+  const toast = useToast();
+
   async function handleUserPhotoSelect() {
-    const photoSelected = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.Images,
-      quality: 1,
-      aspect: [4, 4],
-      allowsEditing: true,
-    });
+    try {
+      const photoSelected = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        quality: 1,
+        aspect: [4, 4],
+        allowsEditing: true,
+      });
 
-    if (photoSelected.canceled) {
-      return;
+      if (photoSelected.canceled) {
+        return;
+      }
+
+      const photoURI = photoSelected.assets[0].uri;
+
+      if (photoURI) {
+        // Hackzinho para apresentar o tamanho da imagem
+        const photoInfo = (await FileSystem.getInfoAsync(photoURI)) as {
+          size: number;
+        };
+
+        // Validando se o tamanho da foto é maior que 5MB
+        if (photoInfo.size && photoInfo.size / 1024 / 1024 > 1) {
+          return toast.show({
+            placement: 'top',
+            render: ({ id }) => (
+              <ToastMessage
+                id={id}
+                action='error'
+                title='Essa imagem é muito grande. Escolha uma de até 5MB.'
+                onClose={() => toast.close(id)}
+              />
+            ),
+          });
+        }
+
+        setUserPhoto(photoURI);
+      }
+    } catch (error) {
+      console.log(error);
     }
-
-    setUserPhoto(photoSelected.assets[0].uri);
   }
 
   return (
