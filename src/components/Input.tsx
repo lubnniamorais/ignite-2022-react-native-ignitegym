51d9014476fcc3363c2345a97a931a1f2151c253
lubnniamorais <lubnnia@gmail.com>
2025-04-29 08:53:48 -0300

    chore: Add errorMessage and isInvalid to input form

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,32 +1,66 @@
 import type { ComponentProps } from 'react';
 
-import { Input as GluestackInput, InputField } from '@gluestack-ui/themed';
+import {
+  Input as GluestackInput,
+  InputField,
+  FormControl,
+  FormControlErrorText,
+} from '@gluestack-ui/themed';
+import { FormControlError } from '@gluestack-ui/themed';
 
 type Props = ComponentProps<typeof InputField> & {
+  errorMessage?: string | null;
+  isInvalid?: boolean;
   isReadOnly?: boolean;
 };
 
-export function Input({ isReadOnly = false, ...rest }: Props) {
+export function Input({
+  isReadOnly = false,
+  errorMessage = null,
+  isInvalid = false,
+  ...rest
+}: Props) {
+  // As duas exclamações fazem com que o valor seja convertido em booleano.
+  // Aqui estamos verificando se há uma mensagem de erro ou se o campo é inválido.
+  // Poderemos usar a constante invalid para apresentar uma mensagem de erro ou dizer
+  // se o campo é inválido.
+  const invalid = !!errorMessage || isInvalid;
+
+  // O FormControl é para controlar o input.
+
   return (
-    <GluestackInput
-      h='$14'
-      borderWidth='$0'
-      borderRadius='$md'
-      $focus={{
-        borderWidth: 1,
-        borderColor: '$green500',
-      }}
-      isReadOnly={isReadOnly}
-      opacity={isReadOnly ? 0.5 : 1}
-    >
-      <InputField
-        px='$4'
-        bg='$gray700'
-        color='$white'
-        fontFamily='$body'
-        placeholderTextColor='$gray300'
-        {...rest}
-      />
-    </GluestackInput>
+    <FormControl isInvalid={invalid} mb='$4' w='$full'>
+      <GluestackInput
+        isInvalid={isInvalid}
+        h='$14'
+        borderWidth='$0'
+        borderRadius='$md'
+        $invalid={{
+          borderWidth: 1,
+          borderColor: '$red500',
+        }}
+        $focus={{
+          borderWidth: 1,
+          borderColor: invalid ? '$red500' : '$green500',
+        }}
+        isReadOnly={isReadOnly}
+        opacity={isReadOnly ? 0.5 : 1}
+      >
+        <InputField
+          px='$4'
+          bg='$gray700'
+          color='$white'
+          fontFamily='$body'
+          placeholderTextColor='$gray300'
+          {...rest}
+        />
+      </GluestackInput>
+
+      <FormControlError>
+        <FormControlErrorText color='$red500'>
+          {errorMessage}
+        </FormControlErrorText>
+      </FormControlError>
+    </FormControl>
   );
 }
