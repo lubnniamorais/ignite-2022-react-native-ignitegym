8ec3bb671f01461f5bf585fff11137bde1b74fb9
lubnniamorais <lubnnia@gmail.com>
2025-05-20 10:13:59 -0300

    feat: Add dependency metro-babel

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,10 +1,12 @@
 import { createContext, useState } from 'react';
 
+import { api } from '@services/api';
+
 import type { UserDTO } from '@dtos/UserDTO';
 
 export type AuthContextDataProps = {
   user: UserDTO;
-  signIn: (email: string, password: string) => void;
+  signIn: (email: string, password: string) => Promise<void>;
 };
 
 type AuthContextProviderProps = {
@@ -19,20 +21,21 @@ export const AuthContext = createContext<AuthContextDataProps>(
 // em outros componentes.
 
 export function AuthContextProvider({ children }: AuthContextProviderProps) {
-  const [user, setUser] = useState({
-    id: '1',
-    name: 'Lubnnia',
-    email: 'lubnnia@example.com',
-    avatar: 'lubnnia.png',
-  });
+  const [user, setUser] = useState<UserDTO>({} as UserDTO);
 
-  function signIn(email: string, password: string) {
-    setUser({
-      id: '',
-      name: '',
-      email,
-      avatar: '',
-    });
+  async function signIn(email: string, password: string) {
+    try {
+      const { data } = await api.post('/sessions', { email, password });
+
+      // O data é a resposta da requisição, que contém as informações do usuário
+      // autenticado. O setUser é uma função que atualiza o estado do usuário
+      // autenticado. O user é o estado que armazena as informações do usuário.
+      if (data.user) {
+        setUser(data.user);
+      }
+    } catch (error) {
+      throw error;
+    }
   }
 
   return (
