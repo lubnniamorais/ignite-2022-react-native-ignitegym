591d92fa7e19be75576ccf433b032fe88e878808
lubnniamorais <lubnnia@gmail.com>
2025-05-24 16:04:42 -0300

    feat: Create storageAuthToken file

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -2,6 +2,7 @@ import { createContext, useEffect, useState } from 'react';
 
 import { api } from '@services/api';
 
+import { storageAuthTokenSave } from '@storage/storageAuthToken';
 import {
   storageUserGet,
   storageUserRemove,
@@ -33,6 +34,32 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
   const [isLoadingUserStorageData, setIsLoadingUserStorageData] =
     useState(true);
 
+  async function storageUserAndToken(userData: UserDTO, token: string) {
+    try {
+      setIsLoadingUserStorageData(true);
+
+      api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+      // O api é uma instância do Axios, que é uma biblioteca para fazer requisições HTTP.
+      // O defaults.headers.common['Authorization'] é um cabeçalho que
+      // é enviado em todas as requisições. O Bearer ${token} é um padrão de autenticação
+      // que usa um token de autenticação. O token é uma string que identifica o usuário
+      // e permite que ele acesse recursos protegidos.
+
+      await storageUserSave(userData);
+      await storageAuthTokenSave(token);
+      setUser(userData);
+    } catch (error) {
+      throw error;
+    } finally {
+      setIsLoadingUserStorageData(false);
+      // O finally é um bloco que sempre será executado, independentemente
+      // de o try ou o catch serem executados. Ele é usado para garantir
+      // que o estado do carregamento seja atualizado corretamente.
+      // Isso é importante para evitar que a aplicação fique travada
+      // em um estado de carregamento infinito.
+    }
+  }
+
   async function signIn(email: string, password: string) {
     try {
       const { data } = await api.post('/sessions', { email, password });
@@ -40,9 +67,12 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
       // O data é a resposta da requisição, que contém as informações do usuário
       // autenticado. O setUser é uma função que atualiza o estado do usuário
       // autenticado. O user é o estado que armazena as informações do usuário.
-      if (data.user) {
-        setUser(data.user);
-        storageUserSave(data.user);
+      if (data.user && data.token) {
+        storageUserAndToken(data.user, data.token);
+        // O storageUserAndToken é uma função que armazena as informações
+        // do usuário e o token de autenticação no AsyncStorage. O AsyncStorage
+        // é uma biblioteca que permite armazenar dados de forma persistente
+        // no dispositivo do usuário.
       }
     } catch (error) {
       throw error;
